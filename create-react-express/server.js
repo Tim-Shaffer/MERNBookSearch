74cd642f819655a5200264064d629a662b9b7a50
Tim-Shaffer <tcshaf24@ptd.net>
2020-05-05 13:49:42 -0400

    updated server to use mongoose and the googlebooks db.  Added a controller file to handle the routes

--- create-react-express/server.js
+++ create-react-express/server.js
@@ -3,6 +3,8 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+var mongoose = require("mongoose");
+
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -11,13 +13,12 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
+// establish MongoDB var based on deployed or local
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true } );
 
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// require the controller file
+require("./controllers/books_controller.js")(app);
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
