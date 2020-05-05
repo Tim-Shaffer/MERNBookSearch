f86268aa46fb8ebe6799ae2c089df2eb777307f4
Tim-Shaffer <tcshaf24@ptd.net>
2020-05-05 13:47:08 -0400

    created mongodb model schema for Books

---
+++ models/Books.js
@@ -0,0 +1,38 @@
+// require necessary package
+var mongoose = require("mongoose");
+
+// Save a reference to the Schema constructor
+var Schema = mongoose.Schema;
+
+// Create a new BookSchema object
+var BookSchema = new Schema({
+
+  title: {
+    type: String,
+    required: true
+  },
+  authors: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+    unique: true
+  }
+  
+});
+
+// Creates an Book model from the schema, using mongoose's model method
+var Book = mongoose.model("Book", BookSchema);
+
+// Export the Book model
+module.exports = ;
