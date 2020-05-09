10f31f76ecb23b929ade5790c4e5267251bc138e
Tim-Shaffer <tcshaf24@ptd.net>
2020-05-09 18:20:24 -0400

    updated styling with a better color pallette

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -14,13 +14,13 @@ class Navbar extends Component {
         <nav className="navbar navbar-expand-md navbar-dark bg-color">
             <div className="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
                 <ul className="navbar-nav mr-auto ml-5">
-                    <li className="nav-item active text-white">
+                    <li className="nav-item active">
                         <a className="nav-link" href="/">Google Books</a>
                     </li>
-                    <li className="nav-item text-white">
+                    <li className="nav-item">
                         <a className="nav-link" href="/search">Search</a>
                     </li>
-                    <li className="nav-item text-white">
+                    <li className="nav-item">
                         <a className="nav-link" href="/saved">Saved</a>
                     </li>
                 </ul>
